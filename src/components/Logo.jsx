f8329c1672cf6f637c045536e90e7f3955dc717d
rizal57 <fahrurrzl5798@gmail.com>
2023-12-28 18:12:02 +0700

    [IMP] Change Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,10 +3,10 @@ import { FaShopify } from "react-icons/fa";
 const Logo = () => {
   return (
     <div>
-      <h1 className="font-bold text-2xl text-teal-500 flex items-center gap-2">
-        Tokol
+      <h1 className="font-bold text-xl text-teal-500 flex items-center gap-2">
+        Shopkuy
         <span>
-          <FaShopify size={25} />
+          <FaShopify size={30} />
         </span>
       </h1>
     </div>
